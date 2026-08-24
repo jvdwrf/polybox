f1cb146dbb4575de9e28fd182665487849ea68fe
jasper <jaspervdwerf@gmail.com>
2026-08-24 18:16:11 -0500

    docs

--- crates/zestors-core/src/handler/handler.rs
+++ crates/zestors-core/src/handler/handler.rs
@@ -15,9 +15,11 @@ impl ExitReason {
     pub fn into_result(self) -> Result<(), Report> {
         match self {
             ExitReason::Normal => Ok(()),
-            ExitReason::InitFailed(e) => Err(e.attach("Handler initialization failed")),
-            ExitReason::InitCancelled => Err(report!("Handler initialization cancelled")),
-            ExitReason::HandlerError(e) => Err(e.attach("Handler encountered an error")),
+            ExitReason::InitFailed(e) => Err(e.attach("handler initialization failed")),
+            ExitReason::InitCancelled => Err(report!(
+                "handler initialization cancelled due to shutdown signal"
+            )),
+            ExitReason::HandlerError(e) => Err(e.attach("handler encountered an error")),
         }
     }
 }
@@ -33,6 +35,10 @@ pub trait Handler: Debug + Sized + Send + 'static {
 
     /// Called when the actor is first spawned, before any messages are processed.
     /// This corresponds to [`ActorStatus::Initializing`].
+    ///
+    /// If a shutdown-signal is received while this method is running, this method
+    /// will be cancelled, and [`Handler::exit`] will be called with
+    /// [`ExitReason::InitCancelled`].
     fn init(
         &mut self,
         _address: &Address<Self::Interface>,
@@ -40,8 +46,11 @@ pub trait Handler: Debug + Sized + Send + 'static {
         async { Ok(()) }
     }
 
-    /// Called when the actor is shutting down, after all messages have been processed.
-    /// This corresponds to [`ActorStatus::ShuttingDown`].
+    /// Called when the actor is about to exit.
+    ///
+    /// This is the final lifecycle hook and is called exactly once, regardless
+    /// of how the actor exits. The [`ExitReason`] describes why the actor is
+    /// exiting.
     fn exit(&mut self, reason: ExitReason) -> impl Future<Output = Result<(), Report>> + Send {
         async { reason.into_result() }
     }
@@ -52,6 +61,11 @@ pub trait Handler: Debug + Sized + Send + 'static {
     /// new messages, and will cause the actor to exit after all messages have
     /// been processed. This method is only for performing any additional actions
     /// that may be needed when the actor is shutting down.
+    ///
+    /// When this method is called, the actor is already in the
+    /// [`ActorStatus::ShuttingDown`] state.
+    ///
+    /// If this method returns an error, [`Handler::exit`] will be called with [`ExitReason::HandlerError`].
     fn on_shutdown(
         &mut self,
         _address: &Address<Self::Interface>,
@@ -63,6 +77,11 @@ pub trait Handler: Debug + Sized + Send + 'static {
     ///
     /// The `Suspend` signal automatically pauses the actor's message processing.
     /// This method is only for performing any additional actions that may be needed when the actor is suspended.
+    ///
+    /// When this method is called, the actor is already in the
+    /// [`ActorStatus::Suspended`] state.
+    ///
+    /// If this method returns an error, [`Handler::exit`] will be called with [`ExitReason::HandlerError`].
     fn on_suspend(
         &mut self,
         _address: &Address<Self::Interface>,
@@ -74,6 +93,11 @@ pub trait Handler: Debug + Sized + Send + 'static {
     ///
     /// The `Resume` signal automatically resumes the actor's message processing.
     /// This method is only for performing any additional actions that may be needed when the actor is resumed.
+    ///
+    /// When this method is called, the actor is already in the
+    /// [`ActorStatus::Running`] state.
+    ///
+    /// If this method returns an error, [`Handler::exit`] will be called with [`ExitReason::HandlerError`].
     fn on_resume(
         &mut self,
         _address: &Address<Self::Interface>,
@@ -83,15 +107,13 @@ pub trait Handler: Debug + Sized + Send + 'static {
 
     /// Called whenever the actor is waiting for a new event to process.
     ///
-    /// When this returns a value, the actor will then call [`Handle::handle`]
+    /// When this returns a value, the actor will then call [`Handle::handle`].
+    ///
+    /// This method must be cancellation-safe.
     fn schedule_next(
         &mut self,
     ) -> impl Future<Output = Result<impl HandledBy<Self>, Report>> + Send {
         pending::<Result<Infallible, _>>()
-    }
-
-    fn children(&self) -> Vec<ChildDescription> {
-        vec![]
     }
 }
 
